d1da416c121fd52f4f6939a4906db9ac6777d53c
Ashish Khandelwal <ashishkhandelwal.me@gmail.com>
2020-03-14 22:30:13 +0530

    'Initial'

---
+++ listings/choices.py
@@ -0,0 +1,56 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10
+}
+
+price_choices ={
+    '100000':'₹1,00,000',
+    '200000':'₹2,00,000',
+    '300000':'₹3,00,000',
+    '400000':'₹4,00,000',
+    '500000':'₹5,00,000',
+    '600000':'₹6,00,000',
+    '700000':'₹7,00,000',
+    '800000':'₹8,00,000',
+    '900000':'₹9,00,000',
+    '1000000':'₹10,00,000'
+}
+
+state_choices = {
+    'AP':'Andhra Pradesh',
+    'AR':'Arunachal Pradesh',
+    'AS':'Assam',
+    'BH':'Bihar',
+    'CH':'Chhattisgarh',
+    'GA':'Goa',
+    'GJ':'Gujarat',
+    'HR':'Haryana',
+    'HP':'Himachal Pradesh',
+    'JH':'Jharkhand',
+    'KN':'Karnataka',
+    'KL':'Kerala',
+    'MP':'Madhya Pradesh',
+    'MH':'Maharashtra',
+    'MN':'Manipur',
+    'MG':'Meghalaya',
+    'MZ':'Mizoram',
+    'NG':'Nagaland',
+    'OD':'Odisha',
+    'PN':'Punjab',
+    'RJ':'Rajasthan',
+    'SK':'Sikkim',
+    'TN':'Tamil Nadu',
+    'TL':'Telangana',
+    'TR':'Tripura',
+    'UK':'Uttarakhand',
+    'UP':'Uttar Pradesh',
+    'WB':'West Bengal'
+}
