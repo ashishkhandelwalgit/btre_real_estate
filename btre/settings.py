509e6bd692d78b424faaf8505053a2b43b7f0a67
ASHISH KHANDELWAL <42886200+ashishkhandelwalgit@users.noreply.github.com>
2020-03-14 22:47:50 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -146,6 +146,6 @@ MESSAGE_TAGS = {
 # Email config
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='shashikhandelwal67@gmail.com'
-EMAIL_HOST_PASSWORD='@$#!$#1996'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
